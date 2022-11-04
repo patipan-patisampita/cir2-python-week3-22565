1db4b841461a25aa169efb9d714a424c373400bd
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-04 13:30:54 +0700

    update

---
+++ assignment.py
@@ -0,0 +1,22 @@
+#Example of Assignment Operator
+a = 10
+
+#1.Assign value
+b = a
+print('1.Assign value b =',b) #10
+
+#2.Add and Assign value
+b += a
+print('2.Add and Assign value b =',b) #20
+
+#3.Subtract and Assign value 
+b -= a
+print('3.Subtract and Assign value b =',b) #10
+
+#4.Multiply and Assign value
+b *= a
+print('Multiply and Assign value b= ', b) #100
+
+#5.Division and Assign value
+b /= a
+print('5.Division and Assign value b =',b)
